626cd9544e89ca306ce3d58add24a971a863933a
habibmir808 <habibmir808@gmail.com>
2022-08-18 13:22:00 +0600

    remove dev dependency

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
-const morgan = require("morgan");
 const pageRouter = require("./router/pages.router");
 const app = express();
 const authRouter = require("./router/auth.router");
 const cors = require("cors");
-const userRouter = require('./router/user.router')
+const userRouter = require("./router/user.router");
 
 app.set("view engine", "ejs");
 app.use(
@@ -13,15 +12,12 @@ app.use(
   express.static("views"),
   express.urlencoded({ extended: false }),
   express.json(),
-  morgan(),
   cookieParser()
 );
 
 app.use("/user/auth", authRouter);
 app.use(pageRouter);
-app.use('/user',userRouter)
-
-
+app.use("/user", userRouter);
 
 app.get("/test", (_req, res) => {
   res.render("testing/index");
